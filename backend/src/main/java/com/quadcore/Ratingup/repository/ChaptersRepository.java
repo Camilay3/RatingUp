e0cb2fc7e3787622ad0ed8a667cbcb54979c3db6
feitosa <davif1605@gmail.com>
2026-05-15 13:16:53 -0300

    fix: Error Response específico para caso de progresso máximo

--- backend/src/main/java/com/quadcore/Ratingup/repository/ChaptersRepository.java
+++ backend/src/main/java/com/quadcore/Ratingup/repository/ChaptersRepository.java
@@ -4,8 +4,10 @@ import com.quadcore.Ratingup.model.book.Chapters;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import java.util.List;
+import java.util.Optional;
 
 public interface ChaptersRepository extends JpaRepository<Chapters, Long> {
     @Query("SELECT c FROM Chapters c LEFT JOIN FETCH c.subtopics ORDER BY c.displayOrder ASC")
     List<Chapters> findAllByOrderbyOrderAsc();
+    Optional<Chapters> findTopByOrderByDisplayOrderDesc();
 }
