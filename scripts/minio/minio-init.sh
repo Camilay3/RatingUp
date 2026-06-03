675877e529fe15f002624abf160da6ac9004d2a6
feitosa <davif1605@gmail.com>
2026-06-03 16:12:28 -0300

    wip: Script init modificado, não sei se mantenho assim

--- scripts/minio/minio-init.sh
+++ scripts/minio/minio-init.sh
@@ -1,31 +1,32 @@
 #!/bin/sh
 set -e
 
-apk add --no-cache curl grep
-curl -sSL https://dl.min.io/client/mc/release/linux-amd64/mc -o /usr/local/bin/mc
-chmod +x /usr/local/bin/mc
+apk add --no-cache curl jq
+
+NEW_FILES=false
 
 upload_if_not_exists() {
   file="$1"
   bucket="$2"
   filename=$(basename "$file")
-  exists=$(curl -s "http://backend:8080/images/exists/$bucket/$filename" | grep -o '"data":[[:space:]]*true' || true)
 
-  if [ -z "$exists" ]; then
-    echo "Upando $filename para $bucket..."
-    if ! curl -f -X POST "http://backend:8080/images/upload/$bucket" \
-      -F "file=@$file;filename=$filename"; then
-      echo "Falha ao upar $filename (HTTP Error)"
-      exit 1
-    fi
+  echo "Verificando $filename..."
+
+  exists=$(curl -s \
+    "http://backend:8080/images/exists/$bucket/$filename" \
+    | jq -r '.data')
+
+  if [ "$exists" = "true" ]; then
+    echo "Imagem já cadastrada"
   else
-    echo "Pulando $filename, já existe"
+    echo "Nova imagem detectada"
+    curl -f -X POST \
+      "http://backend:8080/images/upload/$bucket" \
+      -F "file=@$file;filename=$filename"
+
+    NEW_FILES=true
   fi
 }
-
-mc alias set local http://minio:9000 ${MINIO_ROOT_USER} ${MINIO_ROOT_PASSWORD}
-mc mb --ignore-existing local/book
-mc mb --ignore-existing local/avatars
 
 for file in /images/book/*; do
   [ -e "$file" ] || continue
@@ -37,4 +38,44 @@ for file in /images/avatars/*; do
   upload_if_not_exists "$file" "avatars"
 done
 
-echo "Inicialização concluída"
+
+##!/bin/sh
+#set -e
+#
+#apk add --no-cache curl grep
+#curl -sSL https://dl.min.io/client/mc/release/linux-amd64/mc -o /usr/local/bin/mc
+#chmod +x /usr/local/bin/mc
+#
+#upload_if_not_exists() {
+#  file="$1"
+#  bucket="$2"
+#  filename=$(basename "$file")
+#  exists=$(curl -s "http://backend:8080/images/exists/$bucket/$filename" | grep -o '"data":[[:space:]]*true' || true)
+#
+#  if [ -z "$exists" ]; then
+#    echo "Upando $filename para $bucket..."
+#    if ! curl -f -X POST "http://backend:8080/images/upload/$bucket" \
+#      -F "file=@$file;filename=$filename"; then
+#      echo "Falha ao upar $filename (HTTP Error)"
+#      exit 1
+#    fi
+#  else
+#    echo "Pulando $filename, já existe"
+#  fi
+#}
+#
+#mc alias set local http://minio:9000 ${MINIO_ROOT_USER} ${MINIO_ROOT_PASSWORD}
+#mc mb --ignore-existing local/book
+#mc mb --ignore-existing local/avatars
+#
+#for file in /images/book/*; do
+#  [ -e "$file" ] || continue
+#  upload_if_not_exists "$file" "book"
+#done
+#
+#for file in /images/avatars/*; do
+#  [ -e "$file" ] || continue
+#  upload_if_not_exists "$file" "avatars"
+#done
+#
+#echo "Inicialização concluída"
