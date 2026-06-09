60bb0eb54544f7e1cbb2a0905dab41e813754694
Mateus Pinheiro da Rocha <mateusprocha14@gmail.com>
2026-06-09 12:06:49 -0300

    devolvendo id da session ao criar uma session

--- backend/src/main/java/com/quadcore/Ratingup/dto/board/SubtopicPracticeSessionResponseDTO.java
+++ backend/src/main/java/com/quadcore/Ratingup/dto/board/SubtopicPracticeSessionResponseDTO.java
@@ -1,6 +1,8 @@
 package com.quadcore.Ratingup.dto.board;
 
 public record SubtopicPracticeSessionResponseDTO(
+
+        Long sessionId,
         String fen,
         String status,
         String initialFen
